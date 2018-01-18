89f844972a98dc7e499476bdf72eecbcf240cd68
Tran Dang Tai <taitd@evolableasia.vn>
2018-01-18 10:53:20 +0700

    first commit

---
+++ buddybuild/buddybuild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buddybuild
+//
+//  Created by Tran Dang Tai on 1/18/18.
+//  Copyright © 2018 Tran Dang Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
